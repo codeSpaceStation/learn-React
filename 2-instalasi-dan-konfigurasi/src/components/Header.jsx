93266e120c57192aa4e72f9184d6aad7c3b90bc8
Muhammad Faturahman Bancin <bancin2001@gmail.com>
2025-03-18 12:50:32 +0700

    initial commit & push ☄️

---
+++ 2-instalasi-dan-konfigurasi/src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header({ nama }) {
+	return <h1> Belajar React Bareng {nama ? nama : "Anonym"}☄️</h1>;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-React
+
+this is the repo for code of learning react js
